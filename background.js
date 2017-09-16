b4cb024cd3e0a49fba0bdcd46099f4992e1cda9c
Itay Perl <itay.perl@gmail.com>
2017-09-17 00:48:55 +0300

    inrease result limit in request

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    $.ajax('http://www.gavo.t.u-tokyo.ac.jp/ojad/search/index/display:print/sortprefix:accent/narabi1:kata_asc/narabi2:accent_asc/narabi3:mola_asc/yure:visible/curve:invisible/details:invisible/limit:20/word:' + encodeURIComponent(request.words.join(','))).done(function(data) {
+    $.ajax('http://www.gavo.t.u-tokyo.ac.jp/ojad/search/index/display:print/sortprefix:accent/narabi1:kata_asc/narabi2:accent_asc/narabi3:mola_asc/yure:visible/curve:invisible/details:invisible/limit:500/word:' + encodeURIComponent(request.words.join(','))).done(function(data) {
         var result = parseOJAD(data);
         sendResponse(result);
     });
